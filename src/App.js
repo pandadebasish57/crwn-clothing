faf76b8ef847d3434d0b7d8ae85905a2c1409a7d
Debasish Panda <debasishpanda.beeligible@gmail.com>
2021-04-22 22:14:15 +0530

    setting up the redux

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      currentUser: null,
+      currentUser: null
     };
   }
   unsubscribeFromAuth = null;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import {Provider} from 'react-redux';
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
