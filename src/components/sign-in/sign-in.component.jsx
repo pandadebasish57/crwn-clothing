9415dde739ed6a0d7fe8536f53877e5ad011cabe
Debasish Panda <debasishpanda.beeligible@gmail.com>
2021-04-22 14:11:17 +0530

    finishing sign in handle submit

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -6,7 +6,7 @@ import FormInput from "../form-input/form-input.component";
 
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../firebase/firebase.utils";
+import {auth,signInWithGoogle } from "../firebase/firebase.utils";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -16,9 +16,15 @@ class SignIn extends React.Component {
       password: "",
     };
   }
-  handleSubmit = (event) => {
+  handleSubmit = async event => {
     event.preventDefault();
-
+    const {email,password}=this.state;
+    try{
+      await auth.signInWithEmailAndPassword(email,password);
+      this.setState({email:'',password:''});
+    }catch(error){
+      console.log(error);
+    }
     this.setState({ email: "", password: "" });
   };
   handleChange = (event) => {
