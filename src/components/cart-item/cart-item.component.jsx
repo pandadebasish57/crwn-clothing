2952fb1532a3175bccfae4756ab9ff40c1df4ff3
Debasish Panda <debasishpanda.beeligible@gmail.com>
2021-04-25 21:36:23 +0530

    add item remove item

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,14 +1,17 @@
- import React from 'react';
- import './cart-item.style.scss';
+import React from 'react';
 
- const CartItem = ({ item: { imageUrl, price, name , quantity} }) => (
-   <div className="cart-item">
-     <img src={imageUrl} alt="item" />
-     <div className="item-details">
-       <span className="name">{name}</span>
-       <span className="price">{quantity} * ${price}</span>
-     </div>
-   </div>
- );
+import './cart-item.styles.scss';
 
- export default CartItem;
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
+  <div className='cart-item'>
+    <img src={imageUrl} alt='item' />
+    <div className='item-details'>
+      <span className='name'>{name}</span>
+      <span className='price'>
+        {quantity} x ${price}
+      </span>
+    </div>
+  </div>
+);
+
+export default CartItem;
